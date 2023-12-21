36049c9475b69a13e04648786a56ec5b9202703e
dewalt <ewaltd@gmail.com>
2023-12-21 10:24:37 -0500

    Updates

--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -5,18 +5,24 @@ import {
   CustomersTableType,
   FormattedCustomersTable,
 } from '@/app/lib/definitions';
+import {fetchFilteredCustomers} from "@/app/lib/data";
 
 export default async function CustomersTable({
-  customers,
-}: {
-  customers: FormattedCustomersTable[];
+                                               query,
+                                               currentPage,
+                                             }: {
+  query: string;
+  currentPage: number;
 }) {
+
+  const customers = await fetchFilteredCustomers(query , currentPage);
+
   return (
     <div className="w-full">
-      <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>
-        Customers
-      </h1>
-      <Search placeholder="Search customers..." />
+      {/*<h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>*/}
+      {/*  Customers*/}
+      {/*</h1>*/}
+      {/*<Search placeholder="Search customers..." />*/}
       <div className="mt-6 flow-root">
         <div className="overflow-x-auto">
           <div className="inline-block min-w-full align-middle">
